5fb96f843fddc2c2e3116737e1036ce3d88a246d
LucaMontagnaa <101721205+LucaMontagnaa@users.noreply.github.com>
2022-04-10 20:23:28 -0300

    Initial commit

---
+++ Assets/Script/Ejercicio4.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio4 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    int num3;
+
+    void Start()
+    {
+        num3 = num1 + num2;
+        Debug.Log($"La suma de {num1} y {num2}, da como resultado {num3}");
+    }
+    
+    
+}
